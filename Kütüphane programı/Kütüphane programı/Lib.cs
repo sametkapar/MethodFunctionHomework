9b6fdba23c6438fae312111898249819c010ece2
sametkapar <130076921+sametkapar@users.noreply.github.com>
2024-07-16 13:46:50 +0300

    Homework

---
+++ Kütüphane programı/Kütüphane programı/Lib.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kütüphane_programı
+{
+    internal class Lib
+    {
+        public string bookName;
+        public string writer;
+        public double rentPrc;
+        public double buyPrc;
+
+
+        public string AddName()
+        {
+            Console.WriteLine("Kitap Adını Giriniz: ");
+            string addedBook = Console.ReadLine();
+            return addedBook;
+        }
+        public string AddWrt()
+        {
+            Console.WriteLine("Kitap Yazar Adını Giriniz: ");
+            string addedWrt = Console.ReadLine();
+            return addedWrt;
+        }
+        public double AddRnt()
+        {
+            Console.WriteLine("Kiralama Fiyatını Giriniz: ");
+            double addedRnt = Convert.ToDouble(Console.ReadLine());
+            return addedRnt;
+        }
+        public double AddBuy()
+        {
+            Console.WriteLine("Satış Fiyatını Giriniz: ");
+            double addedBuy = Convert.ToDouble(Console.ReadLine());
+            return addedBuy;
+        }
+    }
+}
+
---
+++ Kütüphane programı/Kütüphane programı/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kütüphane_programı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region Kital Ekleme
+            Lib[] books =new Lib[2];
+            Lib book1 = new Lib();
+            books[0] = book1;
+
+            Lib book2 = new Lib();
+            books[1] = book2;
+            bool devam = true;
+            while (devam == true)
+            {
+                for (int i = 0; i < books.Length; i++)
+                {
+
+                    books[i].bookName = books[i].AddName();
+                    books[i].writer = books[i].AddWrt();
+                    books[i].rentPrc = books[i].AddRnt();
+                    books[i].buyPrc = books[i].AddBuy();
+
+                }
+                Console.WriteLine("Kitap eklemeye devam etmek istiyormusunuz? e/h");
+                string cvp = Console.ReadLine();
+                if (cvp == "e")
+                {
+                    devam = true;
+                    
+                }
+                else
+                { 
+                    devam = false;
+                }
+            }
+            #endregion
+
+            #region Kitap Listesi
+
+            for (int i = 0; i < books.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ") " + books[i].bookName + " " + books[i].writer + " " + books[i].rentPrc + " " + books[i].buyPrc + " TL");
+            }
+            #endregion
+        }
+
+    }
+}
